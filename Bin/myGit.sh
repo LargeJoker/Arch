4af7caa5b910a581d55946a8318da657afd56e4f
LargeJoker <lz1987714@163.com>
2021-01-28 12:34:00 +0800

    change git shell

--- Bin/myGit.sh
+++ Bin/myGit.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+
+# git init
+# git add .
+# git commit -a -m "information"
+# git push -u origin master 
+
+demand(){
+	git status
+	echo "What do you want to do using the GIT?"
+	echo "1.Initializing repository"
+	echo "2.Add files"
+	echo "3.Clone repository"
+	read -p "Please input the index" index
+}
+
+
+
+AddFiles(){
+	if [ $index == '2']
+			read -p "Please input the commit" commit
+			git add .
+			git commit -m ${commit}
+			git push -u origin master
+	fi
+}
+
+InitRep(){
+	if [ $index == '1' ]
+			git init
+	fi
+}
+
 cd ..
 read -p "need to initialize? [Y/n]" option
 if [ ${option} == "Y" -o ${option} == "y" ]
@@ -8,8 +40,8 @@ then
 	git config --global user.email ${email}
 	git config --global user.name ${name}
 
+	git config --global credential.helper store
 fi
-
 
 git add .
 git status
